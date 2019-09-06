5bbe433eab470cfc280220b121b810b3d54f2031
sdwang <sdwang@Ctrip.com>
2019-09-06 14:42:03 +0800

    add manacher algorithm

--- src/main/java/com/wsd/algorithm/string/Manacher.java
+++ src/main/java/com/wsd/algorithm/string/Manacher.java
@@ -4,62 +4,47 @@ import java.util.Arrays;
 
 public class Manacher {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-        manacher("abcba");
+        String str = "abcdcbafabcdck";
+        //String str = "acbbcbds";
+        System.out.println(manacher(str));
     }
 
-    public static void manacher(String str) {
+    public static char[] manacherString(String str){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            sb.append("#");
+            sb.append(str.charAt(i));
+        }
+        sb.append("#");
+        return sb.toString().toCharArray();
+    }
 
-        char[] cStr = str.toCharArray();
-        StringBuffer sBuffer = new StringBuffer();
-        sBuffer.append("#");
-        for (int i = 0; i < cStr.length; i++) {
-            sBuffer.append(cStr[i]);
-            sBuffer.append("#");
+    public static int manacher(String str){
+        if(str == null || str.length() < 1){
+            return 0;
         }
-        int id = 0;
-        int max = 0;
-        int p[] = new int[sBuffer.length()];
-        for (int i = 1; i < sBuffer.length(); i++) {
-
-            if (i < max) {
-                p[i] = Math.min(p[2 * id - i], max - i);
-            } else {
-                p[i] = 1;
+        char[] charArr = manacherString(str);
+        int[] radius = new int[charArr.length];
+        int R = -1;
+        int c = -1;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < radius.length; i++) {
+            radius[i] = R > i ? Math.min(radius[2*c-i],R-i+1):1;
+            while(i+radius[i] < charArr.length && i - radius[i] > -1){
+                if(charArr[i-radius[i]] == charArr[i+radius[i]]){
+                    radius[i]++;
+                }else{
+                    break;
+                }
             }
-            while (i - p[i] >= 0 && i + p[i] < sBuffer.length()
-                    && sBuffer.charAt(i - p[i]) == sBuffer.charAt(i + p[i])) {
-                p[i]++;
+            if(i + radius[i] > R){
+                R = i + radius[i]-1;
+                c = i;
             }
-            if (i + p[i] > max) {
-                max = i + p[i];
-                id = i;
-            }
-
-            System.out.println(Arrays.toString(p));
+            max = Math.max(max,radius[i]);
         }
-        int maxl = 0;
-        int maxid = 0;
-        for (int i = 0; i < p.length; i++) {
-            if (maxl < p[i]) {
-                maxl = p[i];
-                maxid = i;
-            }
-        }
-        //半径包括id本身。id到第一个元素，id-r+1
-        int r = maxl - 1;
-        int start = maxid - r + 1;
-        int end = maxid + maxl - 1;
-        StringBuffer out = new StringBuffer();
-        for (int i = start; i < end; i++) {
-            if (sBuffer.charAt(i) != '#') {
-                out.append(sBuffer.charAt(i));
-            }
-        }
-        System.out.println("最大回文子串长度：" + (maxl - 1) + "\n内容为：" + out.toString());
+        return max-1;
     }
 
 }
