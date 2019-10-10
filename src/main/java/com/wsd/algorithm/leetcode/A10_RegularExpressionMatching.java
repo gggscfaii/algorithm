3773d064a9000fc24626e336d336060a04b6ea9e
544498438 <544498438@qq.com>
2019-10-11 07:51:08 +0800

    regular expression match

--- src/main/java/com/wsd/algorithm/leetcode/A10_RegularExpressionMatching.java
+++ src/main/java/com/wsd/algorithm/leetcode/A10_RegularExpressionMatching.java
@@ -7,46 +7,21 @@ package com.wsd.algorithm.leetcode;
 public class A10_RegularExpressionMatching {
 
     public static boolean isMatch(String s, String p) {
-        if ((s == null || s.equals("")) && (p == null || p.equals(""))) {
-            return true;
+        if (p.isEmpty()) return s.isEmpty();
+        boolean firstmatch = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            return firstmatch && isMatch(s.substring(1), p)
+                    || isMatch(s, p.substring(2));
+        } else {
+            return firstmatch && isMatch(s.substring(1), p.substring(1));
         }
 
-        if ((s == null || s.equals("")) && p != null) {
-            return true;
-        }
-
-        if (p == null || p.equals("")) {
-            return true;
-        }
-
-        int i = 0, j = 0;
-        boolean match = false;
-        while (i < s.length() && j < p.length()) {
-            if (p.charAt(j) == '.' || p.charAt(j) == s.charAt(i)) {
-                match = true;
-            }
-
-            if (p.charAt(j) == '*' && match) {
-                i++;
-                continue;
-            }
-
-            if (match) {
-                j++;
-                i++;
-            } else {
-                j++;
-                match = false;
-            }
-        }
-
-        return i == s.length();
     }
 
     public static void main(String[] args) {
-        System.out.println(isMatch("ab", ".*"));
+        System.out.println(isMatch("aaaaab", ".*"));
         System.out.println(isMatch("aab", "c*a*b"));
-        System.out.println(isMatch("mississippi", "mis*is*p*."));
+        System.out.println(isMatch("mississippi", "mis*is*ip*."));
 
     }
 
