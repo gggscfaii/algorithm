e7663d5563b4be8d2a3674d3ba853caa123e58d4
sdwang <sdwang@Ctrip.com>
2020-01-17 14:47:38 +0800

    add ints test

---
+++ src/main/java/com/wsd/algorithm/basic/IntsTest.java
@@ -0,0 +1,14 @@
+package com.wsd.algorithm.basic;
+
+/**
+ * IntsTest
+ * Created by sdwang on 2020/1/16 15:16.
+ */
+public class IntsTest {
+
+    public static void main(String[] args) {
+        System.out.println(Integer.numberOfLeadingZeros(4));
+        System.out.println(Integer.numberOfTrailingZeros(4));
+        System.out.println(Integer.toBinaryString(~15));
+    }
+}
