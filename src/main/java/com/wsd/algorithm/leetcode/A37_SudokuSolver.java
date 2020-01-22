2768c27e4cec07b9ee020caf549d255614da3a7a
sdwang <sdwang@Ctrip.com>
2020-01-22 19:22:23 +0800

    sudo ku solver

---
+++ src/main/java/com/wsd/algorithm/leetcode/A37_SudokuSolver.java
@@ -0,0 +1,13 @@
+package com.wsd.algorithm.leetcode;
+
+/**
+ * A37_SudokuSolver
+ * Created by sdwang on 2020/1/22 19:17.
+ */
+public class A37_SudokuSolver {
+
+    public static void solveSudoku(char[][] board) {
+
+    }
+
+}
