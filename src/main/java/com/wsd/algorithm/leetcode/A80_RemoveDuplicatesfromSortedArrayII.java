bf8371b72ab56165822a08d6e49927eec05296db
sdwang <sdwang@Ctrip.com>
2020-03-11 18:08:41 +0800

    add remove duplicates

--- src/main/java/com/wsd/algorithm/leetcode/A80_RemoveDuplicatesfromSortedArrayII.java
+++ src/main/java/com/wsd/algorithm/leetcode/A80_RemoveDuplicatesfromSortedArrayII.java
@@ -20,8 +20,53 @@ public class A80_RemoveDuplicatesfromSortedArrayII {
         return pre + 1;
     }
 
+
+    public static int removeDuplicates1(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int pre = 0, cur = 1, n = nums.length;
+        while (cur < n) {
+            boolean hasRepeat = false;
+            while (cur < n && nums[pre] == nums[cur]) {
+                cur++;
+                hasRepeat = true;
+            }
+
+            int preTemp = pre;
+            int curTemp = cur;
+            while (hasRepeat && curTemp < n) {
+                nums[preTemp++] = nums[curTemp++];
+            }
+
+            if (curTemp >= n) {
+                n = preTemp;
+            }
+
+            if (!hasRepeat) {
+                pre++;
+            }
+            cur = pre + 1;
+        }
+        return pre < n ? pre + 1 : -1;
+    }
+
+
     public static void main(String[] args) {
         System.out.println(removeDuplicates(new int[]{1, 1, 1, 2, 2, 3}));
+
+        int[] ints = {1, 1, 1, 2, 2, 3};
+        System.out.println(removeDuplicates1(ints));
+        ArrayUtil.print(ints);
+
+        int[] ints1 = {1, 2, 3, 4, 5, 6};
+        System.out.println(removeDuplicates1(ints1));
+        ArrayUtil.print(ints1);
+
+        int[] ints2 = {1, 1};
+        System.out.println(removeDuplicates1(ints2));
+        ArrayUtil.print(ints2);
+
+
     }
 
 }
