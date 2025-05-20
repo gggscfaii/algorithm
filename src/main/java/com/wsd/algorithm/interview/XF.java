98ab459ef9af5e9909d5d8239fbbee111f32e2e3
sdwang <wangshudong@teddymobile.cn>
2025-05-20 16:52:28 +0800

    线程打印顺序测试

---
+++ src/main/java/com/wsd/algorithm/interview/XF.java
@@ -0,0 +1,26 @@
+package com.wsd.algorithm.interview;
+
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+public class XF {
+    public static void main(String[] args) {
+
+        String filePath = "C:\\data\\book\\AE20221100000_59064374.pdf";
+        try {
+            // 读取文件字节
+            byte[] fileBytes = Files.readAllBytes(Paths.get(filePath));
+
+            // 转换为 Base64
+            String base64Content = Base64.getEncoder().encodeToString(fileBytes);
+
+            // 输出结果
+            System.out.println(base64Content);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
